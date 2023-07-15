4c00386ce0c2dec34453eb62bd659128b28cadc4
zhao.gangshan <zhao.gangshan@yottabyte.cn>
2023-07-16 01:10:31 +0800

    调用KFCFactory

--- src/main/java/com/sadtrain/autotrans/mirai/bots/BotHelper.java
+++ src/main/java/com/sadtrain/autotrans/mirai/bots/BotHelper.java
@@ -1,22 +1,44 @@
 package com.sadtrain.autotrans.mirai.bots;
 
+//import application.miraisignhandler.MiraiSignHandler;
+//import application.miraisignhandler.SignEncryptService;
 import com.sadtrain.autotrans.web.request.AddBotRequest;
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.auth.BotAuthorization;
+import net.mamoe.mirai.internal.spi.EncryptService;
+import net.mamoe.mirai.internal.spi.EncryptServiceContext;
 import net.mamoe.mirai.utils.BotConfiguration;
 import net.mamoe.mirai.utils.LoginSolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import xyz.cssxsh.mirai.tool.FixProtocolVersion;
+import xyz.cssxsh.mirai.tool.KFCFactory;
 
 import java.io.File;
+import java.util.ServiceLoader;
 
 public class BotHelper {
 
     private static Logger logger = LoggerFactory.getLogger(MyBot.class);
     public static Bot newBot(Long botNum,String password){
-        FixProtocolVersion.update(); //先执行FixProtocolVersion的修复，我的插件才能获取正确的协议版本
+//        FixProtocolVersion.sync(); //先执行FixProtocolVersion的修复，我的插件才能获取正确的协议版本
+        ServiceLoader<EncryptService> signEncryptServices = ServiceLoader.load(EncryptService.class);
+        for (EncryptService signEncryptService : signEncryptServices) {
+            System.out.println(signEncryptService.getClass().getName());
+        }
+        KFCFactory.Companion.install();
+        ServiceLoader<EncryptService.Factory> factories = ServiceLoader.load(EncryptService.Factory.class);
+        for (EncryptService.Factory factory : factories) {
+            System.out.println(factory.getClass().getName());
+        }
+//        MiraiSignHandler.register();
+//        KFCFactory kfcFactory = new KFCFactory(); //创建KFC工厂
+//        EncryptServiceContext encryptServiceContext = new EncryptServiceContext(botNum, TypeSafeMap.EMPTY); //创建加密服务上下文
+//        kfcFactory.createForBot();
+//
+//        EncryptService encryptService = kfcFactory.createForBot().create(botNum, encryptServiceContext); //创建加密服务
+//        encryptService.qSecurityGetSign()
         BotFactory.INSTANCE instance = BotFactory.INSTANCE;
         BotConfiguration botConfiguration = new BotConfiguration();
 
